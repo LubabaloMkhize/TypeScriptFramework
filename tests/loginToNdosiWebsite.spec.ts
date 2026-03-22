87dca3aa83af1adec0d6d6573fb524e982be33aa
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2026-03-22 23:07:59 +0200

    Added a succesful login test.

--- tests/loginToNdosiWebsite.spec.ts
+++ tests/loginToNdosiWebsite.spec.ts
@@ -24,3 +24,19 @@ test("Verify page heading", async ({ page }) => {
     await expect(page.getByRole('heading', { name: /Master Test Automation/ }))
         .toBeVisible();
 });
+
+test("Successful login", async ({ page }) => {
+    await page.goto("https://ndosisimplifiedautomation.vercel.app/");
+
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('heading', { name: 'Login to Access Learning Materials'}).isVisible();
+
+    await page.locator('#login-email').fill('Tatalo.Mkhize@example.com');
+
+    await page.locator('#login-password').fill('England@123456');
+
+    await page.locator('#login-submit').click();
+
+    await expect(page.getByRole('heading', { name: /Welcome back/ })).toBeVisible();
+});
