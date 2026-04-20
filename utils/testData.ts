e6d52d56a16fd32af76d2716ae949aaa5a7eac45
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-04-20 15:25:46 +0200

    I have added a step for sending an email.

--- utils/testData.ts
+++ utils/testData.ts
@@ -15,7 +15,7 @@ export const signUpData = {
     validSignUp: {
         firstName:'Ask',
         lastName:'Sis',
-        email:'Sello@gmail.com',
+        email:'Sello1@gmail.com',
         password: 'England@123456',
         confirmPassword:'England@123456',
         group:'2nd Group 2026 (2026)'
